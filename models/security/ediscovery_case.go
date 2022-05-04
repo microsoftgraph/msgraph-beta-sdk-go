c5de6199ca83bdb4e94b23c43a70bca616bab852
Vincent Biret <vibiret@microsoft.com>
2022-05-04 12:53:06 -0400

    - further replacement of escaped names

--- models/security/ediscovery_case.go
+++ models/security/ediscovery_case.go
@@ -35,7 +35,7 @@ type EdiscoveryCase struct {
 // NewEdiscoveryCase instantiates a new ediscoveryCase and sets the default values.
 func NewEdiscoveryCase()(*EdiscoveryCase) {
     m := &EdiscoveryCase{
-        Case: *NewCase_escaped(),
+        Case_escaped: *NewCase_escaped(),
     }
     return m
 }
@@ -77,7 +77,7 @@ func (m *EdiscoveryCase) GetExternalId()(*string) {
 }
 // GetFieldDeserializers the deserialization information for the current model
 func (m *EdiscoveryCase) GetFieldDeserializers()(map[string]func(i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91.ParseNode)(error)) {
-    res := m.Case.GetFieldDeserializers()
+    res := m.Case_escaped.GetFieldDeserializers()
     res["closedBy"] = func (n i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91.ParseNode) error {
         val, err := n.GetObjectValue(ie233ee762e29b4ba6970aa2a2efce4b7fde11697ca9ea81099d0f8269309c1be.CreateIdentitySetFromDiscriminatorValue)
         if err != nil {
@@ -276,7 +276,7 @@ func (m *EdiscoveryCase) GetTags()([]EdiscoveryReviewTagable) {
 }
 // Serialize serializes information the current object
 func (m *EdiscoveryCase) Serialize(writer i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91.SerializationWriter)(error) {
-    err := m.Case.Serialize(writer)
+    err := m.Case_escaped.Serialize(writer)
     if err != nil {
         return err
     }
