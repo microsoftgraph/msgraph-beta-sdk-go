81669d1492c55c81c908e9223dbfdff51515241f
rkodev <43806892+rkodev@users.noreply.github.com>
2022-09-22 11:55:13 +0300

    Adds client service and initialization

---
+++ graph_service_client.go
@@ -0,0 +1,43 @@
+package msgraphbetasdkgo
+
+import (
+	"errors"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	abstractions "github.com/microsoft/kiota-abstractions-go"
+	az "github.com/microsoft/kiota-authentication-azure-go"
+)
+
+type GraphServiceClient struct {
+	*GraphBaseServiceClient
+}
+
+func NewGraphClient(adapter abstractions.RequestAdapter) *GraphServiceClient {
+	client := NewGraphServiceClient(adapter)
+	return &GraphServiceClient{
+		client,
+	}
+}
+
+// NewGraphServiceClientWithCredentials instantiates a new GraphBaseServiceClient with provided credentials and scope
+func NewGraphServiceClientWithCredentials(credential azcore.TokenCredential, scopes []string) (*GraphServiceClient, error) {
+	auth, err := az.NewAzureIdentityAuthenticationProviderWithScopes(credential, scopes)
+	if err != nil {
+		return nil, err
+	}
+
+	adapter, err := NewGraphRequestAdapter(auth)
+	if err != nil {
+		return nil, err
+	}
+
+	client := NewGraphClient(adapter)
+	return client, nil
+}
+
+// GetAdapter returns the client current adapter, Method should only be called when the user is certain an adapter has been provided
+func (m *GraphBaseServiceClient) GetAdapter() abstractions.RequestAdapter {
+	if m.requestAdapter != nil {
+		panic(errors.New("request adapter has not been initialized"))
+	}
+	return m.requestAdapter
+}
