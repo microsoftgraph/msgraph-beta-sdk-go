ae6830c781d5f367a379a4e147192fce91f6aef1
Vincent Biret <vibiret@microsoft.com>
2023-03-28 15:03:36 -0400

    - fixes me shim with properties hoist

--- graph_service_client.go
+++ graph_service_client.go
@@ -55,18 +55,18 @@ func NewGraphServiceClientWithCredentialsAndHosts(credential azcore.TokenCredent
 
 // GetAdapter returns the client current adapter, Method should only be called when the user is certain an adapter has been provided
 func (m *GraphBaseServiceClient) GetAdapter() abstractions.RequestAdapter {
-	if m.requestAdapter == nil {
+	if m.BaseRequestBuilder.RequestAdapter == nil {
 		panic(errors.New("request adapter has not been initialized"))
 	}
-	return m.requestAdapter
+	return m.BaseRequestBuilder.RequestAdapter
 }
 
 // Me provides operations to manage the user singleton.
 func (m *GraphBaseServiceClient) Me() *icd01c84a90833c55ac2309fd7034cb1962c60f59eb1ee2b2cf7b04c708402b6a.UserItemRequestBuilder {
 	urlTplParams := make(map[string]string)
-	for idx, item := range m.pathParameters {
+	for idx, item := range m.BaseRequestBuilder.PathParameters {
 		urlTplParams[idx] = item
 	}
 	urlTplParams["user%2Did"] = "me-token-to-replace"
-	return icd01c84a90833c55ac2309fd7034cb1962c60f59eb1ee2b2cf7b04c708402b6a.NewUserItemRequestBuilderInternal(urlTplParams, m.requestAdapter)
+	return icd01c84a90833c55ac2309fd7034cb1962c60f59eb1ee2b2cf7b04c708402b6a.NewUserItemRequestBuilderInternal(urlTplParams, m.BaseRequestBuilder.RequestAdapter)
 }
