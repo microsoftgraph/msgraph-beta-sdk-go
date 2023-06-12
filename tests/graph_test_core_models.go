34a3b7f64480edae99440d4132806ad6902962b4
Ronald K <43806892+rkodev@users.noreply.github.com>
2023-06-12 15:26:06 +0300

    Adds missing changelog models to the core package (#295)

---
+++ tests/graph_test_core_models.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"github.com/microsoftgraph/msgraph-beta-sdk-go/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCompilationOfAddedModels(t *testing.T) {
+	assert.NotNil(t, models.NewChangeNotification())
+	assert.NotNil(t, models.NewChangeNotificationCollectionResponse())
+	assert.NotNil(t, models.NewChangeNotificationEncryptedContent())
+	assert.NotNil(t, models.CREATED_CHANGETYPE)
+	assert.NotNil(t, models.MISSED_LIFECYCLEEVENTTYPE)
+	assert.NotNil(t, models.NewResourceData())
+	assert.NotNil(t, models.NewResourcePermission())
+	assert.NotNil(t, models.NewResourcePermissionCollectionResponse())
+}
